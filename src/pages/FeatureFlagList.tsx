745c092280a3ae1566b870c13de2d751816b2c0f
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2024-11-22 23:46:12 +0530

    fix: formatting

--- src/pages/FeatureFlagList.tsx
+++ src/pages/FeatureFlagList.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 const FeatureFlagList: React.FC = () => {
   return (
     <div className="container mx-auto mt-20 p-4">
-      <h1 className="text-2xl font-bold mb-4">Feature Flags</h1>
+      <h1 className="mb-4 text-2xl font-bold">Feature Flags</h1>
     </div>
   );
 };
 
-export default FeatureFlagList; 
+export default FeatureFlagList;
